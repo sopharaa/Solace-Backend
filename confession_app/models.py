d4c7a09b7371ed4237c7df7cb05d6b190d874ca0
sopharaa <schhay2@paragoniu.edu.kh>
2026-05-07 00:21:46 +0700

    create table confessions

--- confession_app/models.py
+++ confession_app/models.py
@@ -1,3 +1,23 @@
+import uuid as _uuid
 from django.db import models
+from user_app.models import User
 
-# Create your models here.
+
+class Confession(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=_uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='confessions')
+    title = models.CharField(max_length=255)
+    is_anonymous = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
+    archived_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'confessions'
+
+    def __str__(self):
+        return self.title
